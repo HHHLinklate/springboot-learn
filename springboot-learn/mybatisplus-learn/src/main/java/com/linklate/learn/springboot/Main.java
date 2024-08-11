208340247795350436a910468c99965944ec889a
HHHLinklate <65404171+HHHLinklate@users.noreply.github.com>
2024-08-11 19:18:33 +0800

    init

---
+++ springboot-learn/mybatisplus-learn/src/main/java/com/linklate/learn/springboot/Main.java
@@ -0,0 +1,8 @@
+package com.linklate.learn.springboot;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
